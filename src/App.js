e5327119655822d5b2d88d2e36e1ee6b6ee2d6f0
root <root@LAPTOP-D31HAE9N.localdomain>
2022-04-11 16:52:23 +0200

    added contact form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="app">
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import styles from "./Contact.module.css";
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_7j8cqg3",
+        "template_49q1pwe",
+        form.current,
+        "JavdGnz6ZKe6h2cgr"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <div className={styles.contact} id="Contact">
-      <h3>Contact</h3>
-      <p>Phone: 3714196430</p>
-      <p>Email: joshuaudeh7@gmail.com</p>
+      <div>
+        <h3>Contact</h3>
+        <p>Phone: 3714196430</p>
+      </div>
+      <form ref={form} onSubmit={sendEmail} className={styles.formContainer}>
+        <label>Name</label>
+        <input type="text" name="name" autoComplete="off" />
+        <label>Email</label>
+        <input type="email" name="user_email" autoComplete="off" />
+        <label>Message</label>
+        <textarea name="message" />
+        <button type="submit" className={styles.formBtn}>
+          Send
+        </button>
+      </form>
     </div>
   );
 }
