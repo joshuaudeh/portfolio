0226700735d6c1ed4a9bfc4725cf227b8b7998f7
root <root@LAPTOP-D31HAE9N.localdomain>
2022-04-14 14:56:10 +0200

    ffixed email responsiveness

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,21 +27,23 @@ function Contact() {
 
   return (
     <div className={styles.contact} id="Contact">
-      <div>
+      <div className={styles.cov}>
         <h3>Contact</h3>
         <p>Phone: 3714196430</p>
       </div>
-      <form ref={form} onSubmit={sendEmail} className={styles.formContainer}>
-        <label>Name</label>
-        <input type="text" name="name" autoComplete="off" />
-        <label>Email</label>
-        <input type="email" name="user_email" autoComplete="off" />
-        <label>Message</label>
-        <textarea name="message" />
-        <button type="submit" className={styles.formBtn}>
-          Send
-        </button>
-      </form>
+      <div>
+        <form ref={form} onSubmit={sendEmail} className={styles.formContainer}>
+          <label>Name</label>
+          <input type="text" name="name" autoComplete="off" />
+          <label>Email</label>
+          <input type="email" name="user_email" autoComplete="off" />
+          <label>Message</label>
+          <textarea name="message" rows="5" cols="60" />
+          <button type="submit" className={styles.formBtn}>
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
